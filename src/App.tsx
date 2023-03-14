afa101649db8949dae186c2a84b0afff06f73024
sombriks <sombriks@gmail.com>
2023-03-13 21:16:17 -0300

    bug on navs

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,20 @@ function App() {
     return (
         <Router>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/counter">Counter</Link>
-                <Link to="/counter-classic">Counter (classic)</Link>
-                <Link to="/listrendering">List rendering</Link>
+                <Link to="">Home</Link>
+                <Link to="counter">Counter</Link>
+                <Link to="counter-classic">Counter (classic)</Link>
+                <Link to="listrendering">List rendering</Link>
                 <a target="_blank"
                    href="https://github.com/sombriks/react-studies" 
                    rel="noreferrer">See on github</a>
             </nav>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/counter" element={<Counter/>}/>
-                <Route path="/counter-classic"
+                <Route path="" element={<Home/>}/>
+                <Route path="counter" element={<Counter/>}/>
+                <Route path="counter-classic"
                        element={<CounterClassic/>}/>
-                <Route path="/listrendering"
+                <Route path="listrendering"
                        element={<ListItems items={[
                            {id: 1, label: "One"},
                            {id: 2, label: "two"}]}/>}/>
